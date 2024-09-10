950c2c96eebe01af6d3685ccc0700fa9f5cbf0e0
sadikizgi <sadikizgi@Sadk-MacBook-Pro.local>
2024-09-10 14:44:55 +0300

    first codes

---
+++ tagi/tagiApp.swift
@@ -0,0 +1,17 @@
+//
+//  tagiApp.swift
+//  tagi
+//
+//  Created by Sadık İzgi on 10.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct tagiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
